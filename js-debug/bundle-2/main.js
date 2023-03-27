f68f2919d7ef17ccf4285025af297d17d85a6029
Luigidefilippo <defilippoluigi65@gmail.com>
2023-03-27 17:40:54 +0200

    MAIN COMPLEATE

--- js-debug/bundle-2/main.js
+++ js-debug/bundle-2/main.js
@@ -11,7 +11,7 @@
 
 
 // ESERCIZIO 1
-for (let i = 0; i > 5; i++) {
+for (let i = 0; i < 5; i++) {
     console.log(i);
 }
 
@@ -23,7 +23,8 @@ function addIfEven(num) {
     }
     return num;
 }
-
+console.log(addIfEven(10));
+console.log(addIfEven(11));
 
 // ESERCIZIO 3
 function loopToFive() {
@@ -45,3 +46,45 @@ function displayEvenNumbers() {
     }
 }
 displayEvenNumbers(); // [2,4,6,8]
+
+// Questo codice inizializza una variabile "i" a 0 e ripete il blocco di istruzioni all'interno del ciclo "for" finché "i" è maggiore di 5, stampando il valore di "i" ad ogni iterazione. Tuttavia, il valore iniziale di "i" è già maggiore di 5, quindi il ciclo non viene eseguito.
+
+// Sì, c'è un errore di sintassi. La condizione del ciclo "for" è sbagliata, poiché l'operatore di confronto dovrebbe essere "<" invece di ">". Inoltre, il punto e virgola dopo la condizione del ciclo deve essere sostituito da una virgola.
+
+// Non ci sono errori logici, ma il codice non viene eseguito a causa dell'errore di sintassi.
+
+// // Codice corretto:
+// for (let i = 0; i < 5; i++) {
+// console.log(i);
+// }
+
+    // ESERCIZIO 2 
+
+// Questa funzione prende un numero come argomento e restituisce il numero stesso se è dispari e il numero più 5 se è pari.
+
+// Sì, c'è un errore di sintassi. L'operatore di assegnazione deve essere sostituito dall'operatore di confronto nella condizione "if".
+
+// Non ci sono errori logici.
+
+// // Codice corretto:
+// function addIfEven(num) {
+// if (num % 2 === 0) {
+// return num + 5;
+// }
+// return num;
+// }
+
+// ESERCIZIO 3
+
+// Questa funzione stampa i numeri da 0 a 4 utilizzando un ciclo "for".
+
+// Sì, c'è un errore di sintassi. La condizione del ciclo "for" ha bisogno di punti e virgola al posto delle virgole.
+
+// Non ci sono errori logici.
+
+// // Codice corretto:
+// function loopToFive() {
+// for (let i = 0; i < 5; i++) {
+// console.log(i);
+// }
+// }
